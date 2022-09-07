9b2c085c85c410f14b5c705e30fb28ff6b4e868b
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-08 02:15:41 +0300

    login omponent add

---
+++ src/components/router/RouterComponent.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <nav>
+        <router-link to="/login">Авторизация</router-link>
+        <router-link to="/registration">Регистрация</router-link>
+    </nav>
+
+    <router-view/>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+    
+</style>
