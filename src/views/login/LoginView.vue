19d288706904958aec780c123ec08a4d84316121
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-08 02:57:16 +0300

    view add

---
+++ src/views/login/LoginView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <router-link to="/login/user">Для пользователя</router-link>
+        <router-link to="/login/admin">Для администратора</router-link>
+    </div>
+</template>
+<script>
+
+export default {
+
+    methods: {
+
+    }
+}
+</script>
+<style lang="scss">
+    
+</style>
