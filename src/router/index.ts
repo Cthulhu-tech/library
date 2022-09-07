238e93828f93e41ead70041c2c9ef4ca052d8e35
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-08 02:16:00 +0300

    router add

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,48 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import RegistrationView from '../views/registration/RegistrationView.vue'
+import UserComponent from '../components/admin/AdminComponents.vue'
+import AdminComponent from '../components/user/UserComponents.vue'
+import LoginView from '../views/login/LoginView.vue'
+import HomeView from '../views/home/HomeVIew.vue'
 
-
-const routes: Array<RouteRecordRaw> = []
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/login',
+    name: 'Login',
+    children: [
+      {
+        path: '/login/user',
+        name: 'user',
+        component: UserComponent
+      },
+      {
+        path: '/login/admin',
+        name: 'admin',
+        component: AdminComponent
+      },
+    ],
+    component: LoginView,
+    meta: {
+      needsAuth: false
+    }
+  },
+  {
+    path: '/registration',
+    name: 'Registration',
+    component: RegistrationView,
+    meta: {
+      needsAuth: false
+    }
+  },
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView,
+    meta: {
+      needsAuth: true
+    }
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
