94090a1ab44995da76e5346ff8641bef7a22d66b
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-08 19:21:58 +0300

    admin logic add

--- src/components/admin/AdminComponents.vue
+++ src/components/admin/AdminComponents.vue
@@ -1,12 +1,37 @@
 <template>
-    <div>
-        admin
-    </div>
+    <form @submit.prevent="login">
+        <input type="text" name="name" v-model="user.name" placeholder="имя">
+        <input type="text" name="surname" v-model="user.surname" placeholder="фамилия">
+        <input type="text" name="password" v-model="user.password" placeholder="пароль">
+        <button>Войти</button>
+        <p v-if="error !== 'Token not valid'">{{error}}</p>
+    </form>
 </template>
-<script>
-export default {
-    
-}
+<script lang="ts">
+import { IAdminData } from '../../store/modules/interface'
+import { defineComponent, ref, watch } from 'vue'
+import { useRouter } from 'vue-router'
+import { useStore } from 'vuex'
+
+export default defineComponent({
+    setup(){
+        const store = useStore()
+        const route = useRouter()
+        const error = ref(store.state.jwt.adminError)
+        const user = ref({ name: '', surname: '', password: '' } as IAdminData)
+        const login = () => store.dispatch('loginInAdmin', user.value)
+
+        watch(store.state.jwt, (selection, prevSelection) => {
+            if(selection.error)
+                error.value = selection.error.message
+            if(selection.adminError)
+                error.value = selection.adminError 
+            if(selection.token)
+                route.push('/')
+        })
+        return {login, user, error, store}
+    }
+})
 </script>
 <style lang="scss">
     
